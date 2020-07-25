264af1edc1f7f589fafc4dfaa4a004694162ce97
Joe Spicuzza <spicuzza21@gmail.com>
2020-07-25 12:19:26 -0400

    server test

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
 const { db, sync } = require('./db/db');
+const router = require('./routes')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')))
+app.use('/api', router);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
-
 
 // sync the db then start the server
 sync()
