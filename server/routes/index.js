3e0aa6c94b144674ac50261bf23077d9d39b0232
Joe Spicuzza <spicuzza21@gmail.com>
2020-07-25 12:25:05 -0400

    server test without destructure

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const { User } = require('../db/models/index');
 
 router.get('/users', async (req, res)=>{
     users = await User.findAll();
-    res.status(200).send({users})
+    res.status(200).send(users)
 })
 
 module.exports= router;
