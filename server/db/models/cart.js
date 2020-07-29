9bbeaa6c5cd35d0670bd8367948abb8f63f8b039
Josh Geisler <joshuageisler@gmail.com>
2020-07-29 11:01:44 -0400

    basic remove from cart flow

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -5,58 +5,69 @@ const Product = require('./product');
 const ProductCart = require('./product-cart');
 
 const Cart = db.define('cart', {
-    id: {
-        type: UUID,
-        defaultValue: UUIDV4,
-        primaryKey: true
-    },
-    total: {
-        type: DECIMAL,
-        defaultValue: 0
-    },
-    completed: {
-        type: BOOLEAN,
-        defaultValue: false,
-        allowNull: false
-    }
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true
+  },
+  total: {
+    type: DECIMAL,
+    defaultValue: 0
+  },
+  completed: {
+    type: BOOLEAN,
+    defaultValue: false,
+    allowNull: false
+  }
 });
 
 Cart.prototype.addItem = async function (productId, quantity) {
-    const addedProduct = await Product.findOne({ where: { id: productId } });
-    const productCart = await ProductCart.findOne({
-        where: {
-            productId,
-            cartId: this.id
-        }
-    });
-    if (productCart) {
-        const newQuantity = productCart.quantity += +quantity
-        await productCart.update({
-            quantity: newQuantity
-        });
-    } else {
-        await ProductCart.create({
-            productId: addedProduct.id,
-            cartId: this.id,
-            quantity: quantity
-        })
+  const addedProduct = await Product.findOne({ where: { id: productId } });
+  let productCart = await ProductCart.findOne({
+    where: {
+      productId,
+      cartId: this.id
     }
-    await this.getTotal();
+  });
+  if (productCart) {
+    const newQuantity = productCart.quantity + +quantity
+    await productCart.update({
+      quantity: newQuantity
+    });
+  } else {
+    productCart = await ProductCart.create({
+      productId: addedProduct.id,
+      cartId: this.id,
+      quantity
+    })
+  }
+  await this.addToTotal(productId, productCart);
 }
 
-Cart.prototype.getTotal = async function () {
-    const products = await ProductCart.findAll({ where: { cartId: this.id } });
-    const total = await products.reduce(async (accum, currentProduct) => {
-        const product = await Product.findByPk(currentProduct.productId);
-        return await accum + +product.price * +currentProduct.quantity
-    }, 0)
-    console.log(typeof total, total)
-    await this.update({ total });
+Cart.prototype.addToTotal = async function (id, productCart) {
+  const product = await Product.findOne({ where: { id } });
+  const newTotal = await +this.total + +(product.price * productCart.quantity);
+  await this.update({ total: newTotal });
 }
 
 Cart.prototype.deleteProduct = async function (productId) {
-    const deletedProduct = await ProductCart.findOne({ where: { productId } });
-    await deletedProduct.destroy();
+  const deletedProduct = await ProductCart.findOne({
+    where: {
+      productId,
+      cartId: this.id
+    }
+  });
+  await deletedProduct.destroy();
+  await this.updateTotal();
+}
+
+Cart.prototype.updateTotal = async function () {
+  const products = await ProductCart.findAll({ where: { cartId: this.id } });
+  const total = await products.reduce(async (accum, currentProduct) => {
+    const product = await Product.findByPk(currentProduct.productId);
+    return await accum + +product.price * +currentProduct.quantity
+  }, 0)
+  await this.update({ total });
 }
 
 module.exports = Cart;
--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -3,51 +3,70 @@ const { Router } = require("express");
 const cartRouter = Router();
 const chalk = require('chalk');
 
-const { Cart, Product } = require('../db/models/index');
+const { Cart, Product, ProductCart } = require('../db/models/index');
 
 cartRouter.post('/add/:id', async (req, res) => {
-    try {
-        const { quantity } = req.query;
-        const { id } = req.params;
-        const cart = await Cart.findByPk(req.cart_id);
-        await cart.addItem(id, quantity);
-        const updatedCart = await Cart.findOne({
-            where: {
-                id: req.cart_id,
-            },
-            include: [Product],
-        });
-        console.log(chalk.cyan('Product Added'));
-        res.send(updatedCart);
-    } catch (e) {
-        console.log(e);
-        res.sendStatus(500);
-    }
+  try {
+    const { quantity } = req.query;
+    const { id } = req.params;
+    const cart = await Cart.findByPk(req.cart_id);
+    await cart.addItem(id, quantity);
+    const updatedCart = await Cart.findOne({
+      where: {
+        id: req.cart_id,
+      },
+      include: [Product],
+    });
+    console.log(chalk.cyan('Product Added'));
+    res.send(updatedCart);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 cartRouter.get('/get', async (req, res) => {
-    try {
-        let cart;
-        cart = await Cart.findOne({
-            where: {
-                id: req.cart_id
-            },
-            include: [Product]
-        });
-        if (!cart) {
-            cart = {
-                products: []
-            }
-        }
-        res.send(cart)
-    } catch (e) {
-        console.log(e);
-        res.sendStatus(500);
+  try {
+    let cart;
+    cart = await Cart.findOne({
+      where: {
+        id: req.cart_id
+      },
+      include: [Product]
+    });
+    if (!cart) {
+      cart = {
+        products: []
+      }
     }
+    res.send(cart)
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+cartRouter.delete('/remove/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const cart = await Cart.findByPk(req.cart_id);
+
+    await cart.deleteProduct(id);
+    const updatedCart = await Cart.findOne({
+      where: {
+        id: req.cart_id
+      },
+      include: [Product]
+    });
+    res.send(updatedCart);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 })
 
 
 module.exports = {
-    path: '/cart',
-    router: cartRouter
+  path: '/cart',
+  router: cartRouter
 }
