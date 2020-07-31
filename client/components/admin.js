bff4568a08f2e3861c8cae5801b872295ceeb98b
Joe Spicuzza <spicuzza21@gmail.com>
2020-07-31 19:06:46 -0400

    working admin products

--- client/components/admin.js
+++ client/components/admin.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import AddProductForm from './addProductForm';
 
 class AdminConsole extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to the Admin Console!</h1>
-        <AddProductForm />
         <h3>Please select the element you would like to modify:</h3>
         <Link to="/admin/categories">Categories</Link>
         <Link to="/admin/Products">Products</Link>
--- client/components/adminProducts.js
+++ client/components/adminProducts.js
@@ -1,38 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import store from '../store/reducer';
 import { getProductsThunk } from '../store/productThunks';
+import AddProductForm from './addProductForm';
 
 
 class AdminProducts extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      products: []
-    }
-  }
-
   async componentDidMount() {
     await this.props.getProducts();
-    this.setState({ products: store.getState().products })
   }
 
   render() {
-    const { products } = this.state;
-
+    const { products } = this.props;
     return (
       <div>
+        <h1>Add A Product:</h1>
+        <AddProductForm />
         <h1>Select the product you would like to edit.</h1>
         <div>
-          {products.map(product => {
+          {
+          products.map(product => {
             return (
               <div key={product.id}>
                 <Link to={`/product/${product.id}`} className='title tag is-white is-large'>{product.name}</Link>
               </div>
             )
-          })}
+          })
+          }
         </div>
       </div>
     )
